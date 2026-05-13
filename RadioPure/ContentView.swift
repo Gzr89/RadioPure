05d02d379da574186b16a7a1bb3c14237057e49f
GzrZone <gzrworkmail@163.com>
2026-05-13 19:46:29 +0800

    Initial Commit

---
+++ RadioPure/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RadioPure
+//
+//  Created by 郭忠仁 on 2026/5/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RadioPure/RadioPureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioPureApp.swift
+//  RadioPure
+//
+//  Created by 郭忠仁 on 2026/5/13.
+//
+
+import SwiftUI
+
+@main
+struct RadioPureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
